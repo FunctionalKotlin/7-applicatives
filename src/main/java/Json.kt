ade1f60a4dbafc95c8aa7ecb29c4118832182065
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-05 21:27:13 +0100

    Even the errors are beautiful

--- src/main/java/Json.kt
+++ src/main/java/Json.kt
@@ -1,6 +1,18 @@
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import java.io.StringReader
+import kotlin.reflect.KClass
 
 fun parse(json: String): JsonObject =
     Parser().parse(StringReader(json)) as JsonObject
+
+sealed class ParseError
+
+data class NotFound(val key: String): ParseError()
+
+data class NotCastable<A: Any>(val key: String, val type: KClass<A>): ParseError()
+
+fun notFound(key: String): Failure<NotFound> = Failure(NotFound(key))
+
+fun <A: Any> notCastable(key: String): Failure<NotCastable<A>> =
+    Failure(NotCastable(key, A::class))
