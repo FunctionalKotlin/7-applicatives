9c0a4744dd7712e9270737ed127d0990052f90cb
Alejandro Hernández <info@alejandrohdezma.com>
2017-11-05 20:59:06 +0100

    Nothing to see here...

--- src/main/java/Result.kt
+++ src/main/java/Result.kt
@@ -2,21 +2,21 @@
 
 sealed class Result<out A, out E>
 
-data class Success<out A, out E>(val value: A) : Result<A, E>()
+data class Success<out A>(val value: A) : Result<A, Nothing>()
 
-data class Failure<out A, out E>(val error: E) : Result<A, E>()
+data class Failure<out E>(val error: E) : Result<Nothing, E>()
 
 fun <A, E, B> Result<A, E>.map(transform: (A) -> B): Result<B, E> =
-    flatMap { transform(it).let { Success<B, E>(it) } }
+    flatMap { transform(it).let { Success(it) } }
 
 fun <A, E, B> Result<A, E>.flatMap(
     transform: (A) -> Result<B, E>): Result<B, E> = when(this) {
         is Success -> transform(value)
-        is Failure -> Failure(error)
+        is Failure -> this
     }
 
 fun <A, E> Result<A, E>.ifSuccess(execute: (A) -> Unit) {
     if (this is Success) execute(this.value)
 }
 
-fun <A, E> pure(a: A): Result<A, E> = Success(a)
+fun <A> pure(a: A): Result<A, Nothing> = Success(a)
